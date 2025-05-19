c0d12f985f4c1546dce4786e95a60bbefa87181c
Ze Song <102711605+nnnnnnz@users.noreply.github.com>
2025-05-19 17:38:14 +0800

    Update llm.py

--- agents/system/llm.py
+++ agents/system/llm.py
@@ -1,9 +1,4 @@
-"""
-@Time    : 2023/5/11 14:45
-@Author  : alexanderwu
-@File    : llm.py
-@From    : https://github.com/geekan/MetaGPT/blob/main/metagpt/llm.py
-"""
+
 from .provider.anthropic_api import Claude2 as Claude
 from .provider.openai_api import OpenAIGPTAPI as LLM
 
